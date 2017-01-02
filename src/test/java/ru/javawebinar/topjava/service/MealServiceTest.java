b4074139af8cb7faf010019a6893ce404bb85016
Virgamot <55555-5KaTaRX2>
2017-01-02 21:56:00 +0300

    HW04(with Optional)

--- src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
@@ -1,21 +1,30 @@
 package ru.javawebinar.topjava.service;
 
-import org.junit.Test;
+import org.junit.*;
+import org.junit.rules.ExpectedException;
+import org.junit.rules.Stopwatch;
+import org.junit.rules.TestRule;
+import org.junit.rules.TestWatcher;
+import org.junit.runner.Description;
 import org.junit.runner.RunWith;
+import org.junit.runners.model.Statement;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlConfig;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.util.exception.NotFoundException;
 
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
 
 import static ru.javawebinar.topjava.MealTestData.*;
 import static ru.javawebinar.topjava.UserTestData.ADMIN_ID;
+import static ru.javawebinar.topjava.UserTestData.USER;
 import static ru.javawebinar.topjava.UserTestData.USER_ID;
 
 @ContextConfiguration({
@@ -26,6 +35,36 @@ import static ru.javawebinar.topjava.UserTestData.USER_ID;
 @Sql(scripts = "classpath:db/populateDB.sql", config = @SqlConfig(encoding = "UTF-8"))
 public class MealServiceTest {
 
+    private static String watchedLog = "";
+
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
+
+
+    @Rule
+    public Stopwatch watcher = new Stopwatch() {
+        @Override
+        protected void succeeded(long nanos, Description description) {
+            watchedLog += description.getMethodName() + " successful with timeout (ms): " + TimeUnit.NANOSECONDS.toMillis(nanos) + "\n";
+        }
+
+        @Override
+        protected void failed(long nanos, Throwable e, Description description) {
+            watchedLog += description.getMethodName() + " failed with timeout (ms): " + TimeUnit.NANOSECONDS.toMillis(nanos) + "\n";
+        }
+
+        @Override
+        protected void finished(long nanos, Description description) {
+            System.out.println(String.format("Time out : %d ms\n", TimeUnit.NANOSECONDS.toMillis(nanos)));
+        }
+    };
+
+
+    @AfterClass
+    public static void after() {
+        System.out.println("\n" + watchedLog);
+    }
+
     @Autowired
     private MealService service;
 
@@ -35,8 +74,9 @@ public class MealServiceTest {
         MATCHER.assertCollectionEquals(Arrays.asList(MEAL6, MEAL5, MEAL4, MEAL3, MEAL2), service.getAll(USER_ID));
     }
 
-    @Test(expected = NotFoundException.class)
+    @Test
     public void testDeleteNotFound() throws Exception {
+        expectedException.expect(NotFoundException.class);
         service.delete(MEAL1_ID, 1);
     }
 
@@ -53,20 +93,24 @@ public class MealServiceTest {
         MATCHER.assertEquals(ADMIN_MEAL1, actual);
     }
 
-    @Test(expected = NotFoundException.class)
+    @Test
     public void testGetNotFound() throws Exception {
+        expectedException.expect(NotFoundException.class);
         service.get(MEAL1_ID, ADMIN_ID);
     }
 
     @Test
     public void testUpdate() throws Exception {
+        //Meal updated = service.get(MEAL1_ID,USER_ID);
         Meal updated = getUpdated();
+        updated.setUser(USER);
         service.update(updated, USER_ID);
         MATCHER.assertEquals(updated, service.get(MEAL1_ID, USER_ID));
     }
 
-    @Test(expected = NotFoundException.class)
+    @Test
     public void testUpdateNotFound() throws Exception {
+        expectedException.expect(NotFoundException.class);
         service.update(MEAL1, ADMIN_ID);
     }
 
